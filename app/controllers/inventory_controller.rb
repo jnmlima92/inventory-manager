27fa478a727f68d6845d903d96c4af9a059ee587
Joao Moura <jnmlima92@gmail.com>
2022-03-08 23:59:41 -0300

    Inventory actions

---
+++ app/controllers/inventory_controller.rb
@@ -0,0 +1,53 @@
+class InventoryController < ApplicationController
+  before_action :set_inventory, only: [:update, :show, :destroy]
+  
+  def index
+    inventories = Inventory.all
+    render json: { response: inventories, count: inventories.count }, status: :ok
+  rescue StandardError => e
+    Rails.logger.warn e.message
+    render json: { message: 'Error on get inventories' }, status: :bad_request
+  end
+
+  def show
+    render json: @inventory, status: :ok
+  rescue StandardError => e
+    Rails.logger.warn e.message
+    render json: { message: 'Error on get inventory' }, status: :bad_request
+  end
+  
+  def create
+    inventory = Inventory.create inventory_params
+    render json: inventory, status: :ok
+  rescue StandardError => e
+    Rails.logger.warn e.message
+    render json: { message: 'Error on create inventory' }, status: :bad_request
+  end
+
+  def update
+    @inventory.update!(inventory_params)
+    render json: @inventory, status: :ok
+  rescue StandardError => e
+    Rails.logger.warn e.message
+    render json: { message: 'Error on update inventory' }, status: :bad_request
+  end
+  
+  def destroy
+    @inventory.destroy
+    render json: { message: 'Inventory deleted' }, status: :ok
+  rescue StandardError => e
+    Rails.logger.warn e.message
+    render json: { message: 'Error on delete inventory' }, status: :bad_request
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name)
+  end
+
+  def set_inventory
+    @inventory = Inventory.find params[:id]
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'merchandise_receipts/index'
+  get 'merchandise_receipts/show'
+  get 'merchandise_receipts/create'
   resources :inventory
 end
